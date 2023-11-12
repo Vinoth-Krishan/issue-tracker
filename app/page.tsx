a1f8daefbf78147ca4bc7525f9ef32a4952f7233
vinoth krishan <drkrishphoto@gmail.com>
2023-11-12 14:37:15 +0530

    implement pagination

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
+import { useSearchParams } from "next/navigation";
 import Pagination from "./componets/Pagination";
 
-export default function Home() {
-  return <Pagination itemCount={100} pageSize={10} currentPage={10} />;
+export default function Home({
+  searchParams,
+}: {
+  searchParams: { page: string };
+}) {
+  return (
+    <Pagination
+      itemCount={100}
+      pageSize={10}
+      currentPage={parseInt(searchParams.page)}
+    />
+  );
 }
